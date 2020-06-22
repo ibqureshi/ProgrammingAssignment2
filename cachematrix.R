eca8dc6bd4e2fbe395bbc481c1ce9e8d2b4bdac6
Ibraheem <ibqureshi01@gmail.com>
2020-06-21 21:56:28 -0400

    assignment tbh dont understand that much used the mean cache as a model

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,18 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ## tbh idk what this does i just made it mirror the cachemean
+  a <- NULL
+  set <- function(b) { 
+    x <<- b
+    a <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) a <<- inverse
+  getinverse <- function() a
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  a <- x$getinverse ##the inverse of the passed in matrix is set to a which was nothing before
+  if (!is.null(a)) { ## if a has stuff in it it has cached data
+    message("getting cached data")
+    return(a)
+  }
+  data <- x$get() ## 
+  a <- solve(data, ...) ## solves for inverse
+  x$setinverse(a) ## sets inverse to a
+  a ## returns a
 }
